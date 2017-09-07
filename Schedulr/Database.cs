db90af41abf36ab42b718cc4d613e394fafeee46
Daniel van Bolhuis <zhex@live.dk>
2017-09-07 15:31:30 +0200

    Initial upload

---
+++ Schedulr/Database.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using LiteDB;
+
+namespace Schedulr
+{
+    public class Database
+    {
+        private static string databaseName, target;
+
+        private readonly LiteCollection<User> _users;
+
+        public Database(string dbname)
+        {
+            var db = new LiteDatabase(databaseName);
+            _users = db.GetCollection<User>();
+        }
+        
+        public bool UserExists(string key)
+        {
+           return _users.Exists(u => u.key == key);
+        }
+
+        public List<Session> GetUsersSessions(string key)
+        {
+            if (UserExists(key))
+            {
+                User x = _users.FindOne(u => u.key == key);
+
+                return x.sessions;
+            }
+            
+            return new List<Session>();
+        }
+
+        public User GetUser(string key)
+        {
+            if (UserExists(key))
+            {
+                return _users.FindOne(u => u.key == key);
+            }
+            else
+                return null; //Throw error?
+        }
+
+        public User NewUser()
+        {
+            
+        }
+    }
+    
+    public class User
+    {
+        public string key { get; set; }
+        public List<Job> jobs { get; set; }
+        public List<Session> sessions { get; set; }
+
+
+    }
+
+    public class Job
+    {
+        public decimal hourly { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Session
+    {
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public decimal wage;
+    }
+    
+}
---
+++ Schedulr/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using RedHttpServerCore;
+using RedHttpServerCore.Plugins;
+using RedHttpServerCore.Plugins.Interfaces;
+using RedHttpServerCore.Response;
+
+namespace Schedulr
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // We serve static files, such as index.html from the 'public' directory
+            var server = new RedHttpServer(5000, "public");
+            var startTime = DateTime.UtcNow;
+            var db = new Database("WorkTimeDatabaseHashboiii");
+
+            // We log to terminal here
+            var logger = new TerminalLogging();
+            server.Plugins.Register<ILogging, TerminalLogging>(logger);
+            
+            
+            server.Get("/hey", async (req, res) =>
+            {
+                
+                await res.SendString("Hi my dude");
+                
+            });
+
+            // URL param demo
+            server.Get("/:param1/:paramtwo/:somethingthird", async (req, res) =>
+            {
+                await res.SendString($"URL: {req.Params["param1"]} / {req.Params["paramtwo"]} / {req.Params["somethingthird"]}");
+            });
+
+            // Redirect to page on same host
+            server.Get("/redirect", async (req, res) =>
+            {
+                await res.Redirect("/redirect/test/here");
+            });
+
+            // Save uploaded file from request body 
+            Directory.CreateDirectory("./uploads");
+            server.Post("/upload", async (req, res) =>
+            {
+                if (await req.SaveBodyToFile("./uploads"))
+                {
+                    await res.SendString("OK");
+                    // We can use logger reference directly
+                    logger.Log("UPL", "File uploaded");
+                }
+                else
+                    await res.SendString("Error", status: 413);
+            });
+
+            server.Get("/file", async (req, res) =>
+            {
+                await res.SendFile("testimg.jpeg");
+            });
+
+            // Using url queries to generate an answer
+            server.Get("/hello", async (req, res) =>
+            {
+                var queries = req.Queries;
+                var firstname = queries["firstname"];
+                var lastname = queries["lastname"];
+                await res.SendString($"Hello {firstname} {lastname}, have a nice day");
+            });
+
+            // Rendering a page for dynamic content
+            server.Get("/serverstatus", async (req, res) =>
+            {
+                await res.RenderPage("./pages/statuspage.ecs", new RenderParams
+                {
+                    { "uptime", DateTime.UtcNow.Subtract(startTime).TotalHours },
+                    { "versiom", RedHttpServer.Version }
+                });
+            });
+            
+            
+
+            server.Start();
+            
+            while (true)
+            {
+                Console.Read();
+            }
+        }
+    }
+}
